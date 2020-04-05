6cdabbc03539eb324bef65a2b456b119f72b6dac
“cecvak” <c.vakaloudi@gmail.com>
2020-04-05 13:18:29 +0200

    two left to solve

--- april_04/src/script.js
+++ april_04/src/script.js
@@ -1,6 +1,25 @@
 // HOMEWORK
 // 1. Write a calculator. Request 2 numbers and an operation as a symbol (+ - * /). 
 // The program solves it depending on the operation and shows the result.
+// FINISHED!!
+// let calcNumber1 = +prompt('Enter a number.');
+// let calcNumber2 = +prompt('Enter another number.');
+// let operation = prompt('What do you want to do with the numbers? Enter + - * /');
+// let result;
+
+// if (operation == '+') {
+//     result = calcNumber1 + calcNumber2;
+// }
+//     else if (operation == '-') {
+//         result = calcNumber1 - calcNumber2;
+//     }
+//     else if (operation == '*') {
+//         result = calcNumber1 * calcNumber2;
+//     }
+//     else if (operation == '/') {
+//         result = calcNumber1 / calcNumber2;
+//     }
+//     console.log(`${calcNumber1} ${operation} ${calcNumber2} = ${result}`);
 
 
 // 2. Count the factorial of the number given by a user. For example, 4! is 
@@ -37,6 +56,7 @@
 // CLASSWORK PART 1
 // 1. Request a user’s age and define whether he/she is an infant (0–2), a child (3-11), a teen (12–18), 
 // an adult (19-60) or a senior citizen (60– ...). Display the result in the console.
+// FINISHED!
 
 // let age = prompt('What is your age?');
 
@@ -56,31 +76,36 @@
 
     // 2. Request a number from 0 to 9 from a user and display the symbol that is placed on that keyboard button 
     // (for example, 1–!, 2–@, 3–# etc). Display a result in the console.
+    // FINISHED!
 
 // let number = prompt('Pick a number between 0 and 9');
 //     if (number == 1) {
 //         console.log('!');
 //     } else if (number == 2) {
-    //     console.log('"');
-    // } else if (number == 3) {
-    //     console.log('#');
-    // } else if (number == 4) {
-    //         console.log('¤');
-    // } else if (number == 5) {
-    //         console.log('%');
-    // } else if (number == 6) {
-    //         console.log('&');
-    // } else if (number == 7) {
-    //         console.log('/');
-    // } else if (number == 8) {
-    //         console.log('(');
-    // } else if (number == 9) {
-    //         console.log(')');
-    // }
+//         console.log('"');
+//     } else if (number == 3) {
+//         console.log('#');
+//     } else if (number == 4) {
+//             console.log('¤');
+//     } else if (number == 5) {
+//             console.log('%');
+//     } else if (number == 6) {
+//             console.log('&');
+//     } else if (number == 7) {
+//             console.log('/');
+//     } else if (number == 8) {
+//             console.log('(');
+//     } else if (number == 9) {
+//             console.log(')');
+//     } else if (number == 0) {
+//             console.log('=');
+//     }
+
 
 
     // 3. Ask a user 3 questions, each with 3 answer options. Write these options also in the prompt window, after a question. 
     // The user gets 2 points for each correct answer. Show the earned points after the test.
+    // FINISHED!
 // let question1 = prompt('What is the capital of Sweden? Stockholm, Oslo or Copenhagen?');
 // let question2 = prompt('What is the capital of the UK? Amsterdam, Rome or London?');
 // let question3 = prompt('What is the capital of Finland? Berlin, Helsinki or Vienna?');
@@ -96,12 +121,22 @@
 //     points = points + 2;
 // }
 
-// console.log(points);
+// console.log(`You get ${points} points!`);
 
 
-    //4.  Request a number from a user, and define whether it is positive, negative, or zero. 
-    // Display a resulting message as an alert.
-// let number2 = prompt('Enter a number');
+//4.  Request a number from a user, and define whether it is positive, negative, or zero. 
+// Display a resulting message as an alert.
+
+// let userNumber = +prompt('Enter a number.');
+// if (userNumber == 0) {
+//     alert ('You entered zero!');
+// }
+//     else if (userNumber > 0) {
+//         alert ('You entered a positive number!');
+//     }
+//     else if (userNumber < 0) {
+//         alert ('You entered a negative number!');
+//     }
 
 
 
@@ -113,18 +148,6 @@
 // } else {
 //     console.log('Hm... Is that even possible?');
 // }
-
-// let isAdult;
-// let age = prompt('How old are you?');
-
-// if (age > 18) {
-//     isAdult = true;
-// } else {
-//     isAdult = false;
-// }
-
-// let age = prompt('How old are you?');
-// let isAdult = age > 18 ? true : false; // let isAdult = age > 18;
 
 
 // CLASSWORK 2
@@ -143,7 +166,33 @@
 //     case '3':
 //         document.write('March');
 //         break;
-//     // etc etc
+//     case '4':
+//         document.write('April');
+//         break;
+//     case '5':
+//         document.write('May');
+//         break;
+//     case '6':
+//         document.write('June');
+//         break;
+//     case '7':
+//         document.write('July');
+//         break;
+//     case '8':
+//         document.write('August');
+//         break;
+//     case '9':
+//         document.write('September');
+//         break;
+//     case '10':
+//         document.write('October');
+//         break;
+//     case '11':
+//         document.write('November');
+//         break;
+//     case '12':
+//         document.write('December');
+//         break;
 //     default:
 //         document.write('That is not a month!');
 //      }
@@ -163,25 +212,26 @@
 
 //3.  One task using do while loop:
 // Suggest a user solve 2 + 2 * 2 until he/she does it right.
-// let solution = 0;
-// do {
-//     solution = prompt('What is 2 + 2 * 2?');
-// }
-//     while (solution != 8);
+// See above for solution.
 
 //4. And one task using for loop:
 // Count sheep. Request a non-negative integer, 3 for example, 
 // and print a string with a murmur: “1 sheep...2 sheep...3 sheep...” 
 // to the page using document.write(). If a user types a negative number in, 
 // alert an error.
-// let sheep = prompt('Enter number of sheep (non-negative number');
-// var i;
-//  for (i = 0; i <= sheep; i++) {
-//      document.write(+i +' sheep...');
-//     if (sheep <0 ) {
-         
-//         document.write('Error! You entered a negative number.');
-//         // break;
-//      }   
-//  }
+// See above for solution.
+
+
+// Examples:
+// let isAdult;
+// let age = prompt('How old are you?');
+
+// if (age > 18) {
+//     isAdult = true;
+// } else {
+//     isAdult = false;
+// }
+
+// let age = prompt('How old are you?');
+// let isAdult = age > 18 ? true : false; // let isAdult = age > 18;
 
