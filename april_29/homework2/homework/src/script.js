9ca127f04dbcf78e0c7a9bf129f2a159a128ec45
“cecvak” <c.vakaloudi@gmail.com>
2020-05-03 17:13:47 +0200

    organising files

---
+++ april_29/homework2/homework/src/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $('.slider').slick({
+    //   setting-name: setting-value
+    dots: true,
+    // nextArrow: '<span class="material-icons">navigate_next</span>',
+    // prevArrow: '<span class="material-icons">navigate_before</span>',
+    arrows: true,
+        // prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+        // nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
+});
+  });
+      
+
+//  <span class="material-icons">navigate_next</span>
+
+// <span class="material-icons">navigate_before</span> 
