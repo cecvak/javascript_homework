0ec8eba67492302315a02e99d445ced667f6ba86
“cecvak” <c.vakaloudi@gmail.com>
2020-05-11 15:07:43 +0200

    fixed mistakes and made a list

--- may_09/homework/src/script.js
+++ may_09/homework/src/script.js
@@ -7,37 +7,39 @@
 // Store the task list as a stringified array. 
 // You can use jQuery to simplify DOM manipulations.
 
-// let tasks = '{"tasks":[{"task":"clean house"} ]}';
-// console.log(tasks);
 
-let tasks = [];
-tasks = JSON.parse(localStorage.getItem('tasks'));
-console.log(tasks);
+let getTasks = localStorage.getItem('tasks');   //get items as string from local storage
+// console.log(getTasks); 
+console.log(typeof getTasks);   //string
+let resultTasks = getTasks ? JSON.parse(getTasks) : [];     //parse string to object and store in array
+console.log(typeof resultTasks);    //object
+console.log(resultTasks);   // displays array of objects
 
 $('form').submit(function(event) {
-    // event.preventDefault();
+    // event.preventDefault();      //prevent page from reloading
 
-    let task = $('#task').val();
-    tasks.push({'task' : task});
+    let task = $('#task').val();    //get input value from form
+    console.log(task);  
+    // resultTasks.push({'task' : task});  //push input value to array
+    resultTasks.push(task);
     // console.log(tasks);
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-
-    // console.log(localStorage);
-    
+    localStorage.setItem('tasks', JSON.stringify(resultTasks));     //store in local storage as string
 })
 
 
-$(function() {
-    let listContainer = $('<div></div>');
 
-    let taskString = "";
-        $.each(tasks, function (key, value) {
-            taskString = (value.task + '<br>');
-            listContainer.append(taskString);
-            console.log(taskString);
+$(function() {
+
+    let list = $('<ul></ul>');                          // create list
+    // let list = document.getElementById('task-list');
+        $.each(resultTasks, function (key, value) {     //go through array and append as list items
+            list.append(`                               
+                <li>${value}</li>
+                `);
+
     });
 
-    $('.output-container').append(listContainer);
-
+    $('.output-container').append(list);                // append list to div
+   
 }
 )
